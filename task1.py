2b1ce77fcf744cec764e8e4ca4bbcec73d2f92e6
abbeljatmiko <93469927+abbeljatmiko@users.noreply.github.com>
2022-11-13 17:52:59 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+n = "Abbel Jatmiko"
+j = "Teknik Elektro"
+u = "Institut Teknologi Bandung"
+a = "Alasan saya ingin masuk GDSC ITB 2022 karena saya ingin mengembangkan software skills saya karena nantinya dunia akan berkembang menjadi lebih modern dan saya rasa bahwa software skills ini sangat penting untuk dipelajari"
+
+print("Nama :", n)
+print("Jurusan :", j)
+print("Universitas :", u)
+print("Alasan ingin masuk GDSC :", a)
